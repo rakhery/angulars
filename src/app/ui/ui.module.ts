2c8759b913a1529e90383be472154e023eeb09b5
Christian RAKOTO <crakoto@yahoo.fr>
2022-09-21 15:14:15 +0200

    creation ui component avec --export

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Ui2Component } from './components/ui2/ui2.component';
+import { UiComponent } from './components/ui/ui.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [Ui2Component, UiComponent],
+  exports: [
+    Ui2Component,
+    UiComponent
+  ],
   imports: [
     CommonModule
   ]
