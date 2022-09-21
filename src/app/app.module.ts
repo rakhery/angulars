071e875d3837598e3c80bf43b224f24f68803010
Christian RAKOTO <crakoto@yahoo.fr>
2022-09-21 11:31:34 +0200

    Ajouter les modules qu'on a besoin prioritairement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
+// importer les classes qu'on a besoin
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {IconsModule} from './icons/icons.module';
+import {LoginModule} from './login/login.module';
 // decorateur des modules
 //
 @NgModule({
-  declarations: [
+  declarations: [   // Verifier si le composant dans le module
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, IconsModule, LoginModule
   ],
   providers: [],
-  bootstrap: [AppComponent]  // Demarrer l'applicat° avec AppComponent
+  bootstrap: [AppComponent]  // Demarrer l'applicat° avec AppComponent et a un rôle
 })
 export class AppModule { }
