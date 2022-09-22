cad077c2949b4d2bc6cd8149971be839897afc6e
Christian RAKOTO <crakoto@yahoo.fr>
2022-09-22 09:05:20 +0200

    Branch App component

---
+++ src/app/core/components/tchat/create-canal/create-canal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-canal',
+  templateUrl: './create-canal.component.html',
+  styleUrls: ['./create-canal.component.scss']
+})
+export class CreateCanalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
