30cb1defba7c80226858f1e11074b54515716624
Christian RAKOTO <crakoto@yahoo.fr>
2022-09-23 10:43:58 +0200

    loadAllStrategy dans module app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {PreloadAllModules, PreloadingStrategy, RouterModule, Routes} from '@angular/router';
 import {PageSignInComponent} from './login/pages/page-sign-in/page-sign-in.component';
 import {PageNotFoundComponent} from './page-not-found/pages/page-not-found/page-not-found.component';
 
@@ -23,7 +23,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{preloadingStrategy:PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
