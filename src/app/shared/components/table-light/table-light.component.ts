00d2c2de0fa3378c94ced6aec948d9d21ae9ddf3
Christian RAKOTO <crakoto@yahoo.fr>
2022-09-26 16:36:57 +0200

    Solution pour rendre réutilisable avec ng-content

---
+++ src/app/shared/components/table-light/table-light.component.ts
@@ -0,0 +1,26 @@
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-table-light',
+  templateUrl: './table-light.component.html',
+  styleUrls: ['./table-light.component.scss']
+})
+export class TableLightComponent implements OnInit,OnChanges {
+  @Input() public headers!:string[];
+  @Input() public collection!:any;
+  constructor() {
+    console.log(this.headers);
+    console.log(this.collection);
+  }
+
+  ngOnInit(): void {
+    console.log(this.headers);
+    console.log(this.collection);
+  }
+
+  ngOnChanges(): void {
+    console.log(this.headers);
+    console.log(this.collection);
+  }
+
+}
